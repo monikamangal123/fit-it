259a00703cd32cab6b062e037618521228d406db
MonikaM <monika.mangal@confirmit.com>
2018-05-06 23:33:41 +0200

    commented database setup temp

--- src/server/app.js
+++ src/server/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Client } = require('pg');
+/* const { Client } = require('pg');
 const client = new Client({
     host: '127.0.0.1',
     port: 5432,
     user: '',
     password: '',
     database: 'fitit'
-});
+}); */
 
 const app = express();
 
@@ -21,13 +21,13 @@ const port = 4000;
 
 app.post('/submit', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    client.connect();
+   // client.connect();
 
-    const insert = 'INSERT INTO activity_details (name, activitytype, kilometer) VALUES ($1, $2, $3) RETURNING id';
+   /* const insert = 'INSERT INTO activity_details (name, activitytype, kilometer) VALUES ($1, $2, $3) RETURNING id';
     client.query(insert, [req.body.name, req.body.activityType, req.body.km], (err, res) => {
         console.log(err ? err.stack : res.rows[0].id);
         client.end()
-    });
+    }); */
 
     res.send(JSON.stringify({status: 'success'}));
 });
